494f35c1197068f640cd5f0896c2f0745150e1ee
SangyunOck <demibun123@naver.com>
2023-04-27 01:31:13 +0900

    🎨 bundler types userops 구현

--- crates/bundler-types/tests/user_operation.rs
+++ crates/bundler-types/tests/user_operation.rs
@@ -1,9 +1,6 @@
 #[cfg(test)]
 pub mod test {
-    use bundler_types::user_operation::UserOperation;
 
     #[test]
-    fn test_fail_user_operation_constructor() {
-
-    }
+    fn test_fail_user_operation_constructor() {}
 }
