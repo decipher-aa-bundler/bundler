3ab8fe5105d4303c34d240cdf730c26d4b7cabcb
SangyunOck <demibun123@naver.com>
2023-05-13 12:23:18 +0900

    ✨ eyre 도입

--- crates/bundler/src/rpc/errors.rs
+++ crates/bundler/src/rpc/errors.rs
@@ -1,12 +1,20 @@
-use crate::ethereum::errors::EthereumError;
-use bundler_types::error::BundlerTypeError;
+use actix_web::body::BoxBody;
+use actix_web::http::StatusCode;
+use actix_web::{HttpResponse, ResponseError};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum RpcError {
-    #[error(transparent)]
-    BundlerTypeError(#[from] BundlerTypeError),
+    #[error("{0}")]
+    Error(eyre::Report),
+}
 
-    #[error(transparent)]
-    EthereumError(#[from] EthereumError),
+impl ResponseError for RpcError {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::BAD_REQUEST
+    }
+
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        HttpResponse::with_body(StatusCode::BAD_REQUEST, BoxBody::new(self.to_string()))
+    }
 }
