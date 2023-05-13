706ba4ab1b08a8a2fca0e2e8d8e9c038fd77fe8c
SangyunOck <demibun123@naver.com>
2023-05-13 12:24:19 +0900

    ✨ estimate_user_ops_gas 구현

--- crates/bundler/src/rpc/handler/user_ops.rs
+++ crates/bundler/src/rpc/handler/user_ops.rs
@@ -1,22 +1,23 @@
-use crate::rpc::models::UserOps;
+use crate::rpc::models::{EstimateUserOpsGasResponse, UserOps};
 use crate::rpc::types::BundlerClient;
+use crate::rpc::errors::RpcError;
 
-use actix_web::dev::Response;
-use actix_web::{post, web, Error, Responder};
+use actix_web::{post, web, Responder};
 
 #[post("/{ep_addr}")]
 pub async fn estimate_user_ops_gas(
     body: web::Json<UserOps>,
     path: web::Path<String>,
     client: web::Data<BundlerClient>,
-) -> Result<impl Responder, Error> {
+) -> Result<impl Responder, RpcError> {
     let user_ops = body.into_inner();
     let ep_addr = path.into_inner();
 
-    client
+    let gas = client
         .bundler_service
-        .estimate_user_ops_gas(user_ops, &ep_addr)
-        .await;
+        .estimate_user_ops_gas(&user_ops, &ep_addr)
+        .await
+        .map_err(RpcError::Error)?;
 
-    Ok(Response::ok())
+    Ok(web::Json(EstimateUserOpsGasResponse::new(gas)))
 }
